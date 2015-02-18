149603bd98e7fe6fb23c6f5621c26b024b1eb5b8
Triskell95 <tdasilva@et.esiea.fr>
2015-02-18 14:18:10 +0100

    Seconde version du TextFormalizer (date implémentée). A tester dès que possible !

--- Archi_Log/src/com/esiea/logger/TextFormalizer.java
+++ Archi_Log/src/com/esiea/logger/TextFormalizer.java
@@ -1,13 +1,25 @@
 package com.esiea.logger;
 
+import java.util.Calendar;
+
+
 public class TextFormalizer {
 
 	public static String formalized(String string, State state) {
 		
+		//récupération de la date
+		Calendar cal = Calendar.getInstance();		
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH);
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		int hour = cal.get(Calendar.HOUR);
+		int minute = cal.get(Calendar.MINUTE);
+		String date = new String(year+"/"+month+"/"+day+"-"+hour+":"+minute);
 		
-		String formattedString = new String("[Logger."+state+"] [ "+string);
+		//Formattage de la string
+		String formattedString = new String("[Logger."+state+"] ["+date+"] "+string);
 		
-		System.out.println(formattedString);
+		//System.out.println(formattedString);
 		
 		return formattedString;
 	}
